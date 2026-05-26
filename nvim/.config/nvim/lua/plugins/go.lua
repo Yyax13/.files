91b8c12a2795cfe5a39ab75185bb1945ed4c0fa5
Yyax13 <Yyax13@proton.me>
2026-05-26 15:25:34 -0300

    chore(nvim/mason): Updated Mason plugin repository to mason-org

--- nvim/.config/nvim/lua/plugins/go.lua
+++ nvim/.config/nvim/lua/plugins/go.lua
@@ -2,8 +2,8 @@ return {
     {
         "neovim/nvim-lspconfig",
         dependencies = {
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
+            "mason-org/mason.nvim",
+            "mason-org/mason.nvim",
         },
         config = function()
             require("mason").setup()
