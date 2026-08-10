e6d0c2519a9c8c66b3ddf1040f947bf20d50af82
Yyax13 <Yyax13@proton.me>
2026-08-10 13:33:23 -0300

    chore: update desktop configs and remove vendored oh-my-zsh

--- custom-binaries/.bin/record-toggle.sh
+++ custom-binaries/.bin/record-toggle.sh
@@ -5,12 +5,21 @@ OUTDIR="$HOME/Videos/Recordings"
 mkdir -p "$OUTDIR"
 
 if [ -f "$PIDFILE" ]; then
-    kill $(cat "$PIDFILE")
-    rm "$PIDFILE"
-    notify-send "Recording stopped"
+  kill $(cat "$PIDFILE")
+  rm "$PIDFILE"
+  notify-send "Recording stopped"
 else
-    FILE="$OUTDIR/rec-$(date +%Y-%m-%d_%H-%M-%S).mp4"
-    wf-recorder -f "$FILE" &
-    echo $! > "$PIDFILE"
+  defaultMonitor="$(pactl get-default-sink).monitor"
+  FILE="$OUTDIR/rec-$(date +%Y-%m-%d_%H-%M-%S).mp4"
+
+  if pactl list short sources | awk '{print $2}' | grep -Fxq "$defaultMonitor"; then
+    wf-recorder --audio="$defaultMonitor" -f "$FILE" &
+    echo $! >"$PIDFILE"
     notify-send "Recording started"
+  else
+    wf-recorder -f "$FILE" &
+    echo $! >"$PIDFILE"
+    notify-send "Recording started"
+  fi
+
 fi
