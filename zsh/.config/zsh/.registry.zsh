a673f3ef6b14a24835ce74a83c7cb02e76430037
Yyax13 <Yyax13@proton.me>
2026-08-13 17:31:26 -0300

    feat(config): Created new Zsh prompt theme and updated wallpapers

--- zsh/.config/zsh/.registry.zsh
+++ zsh/.config/zsh/.registry.zsh
@@ -2,3 +2,4 @@ source $HOME/.config/zsh/aliases.zsh
 source $HOME/.config/zsh/network.zsh
 source $HOME/.config/zsh/language.zsh
 source $HOME/.config/zsh/objdump.zsh
+source $HOME/.config/zsh/themes/wffn.zsh-theme
